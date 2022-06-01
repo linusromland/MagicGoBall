b1c1f9c0a600f2e2ce3de8aa063836bc8de32d5e
Linus Romland <romlandlinus@gmail.com>
2022-06-01 13:52:35 +0200

    Replies with a random reply (same reply if same string)

--- bot/main.go
+++ bot/main.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"math/rand"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
+var (
+	prefix  string
+	replies = []string{
+		"it is certain.",
+		"it is decidedly so.",
+		"without a doubt.",
+		"yes definitely.",
+		"you may rely on it.",
+		"as I see it, yes.",
+		"most likely.",
+		"outlook good.",
+		"yes.",
+		"signs point to yes.",
+		"reply hazy, try again.",
+		"ask again later.",
+		"better not tell you now.",
+		"cannot predict now.",
+		"concentrate and ask again.",
+		"don't count on it.",
+		"my reply is no.",
+		"my sources say no.",
+		"outlook not so good.",
+		"very doubtful.",
+	}
+)
 
 func main() {
 
 	//Load .env file
-	err := godotenv.Load()
+	godotenv.Load()
+
+	if os.Getenv("PREFIX") == "" {
+		prefix = "!"
+	} else {
+		prefix = os.Getenv("PREFIX")
+	}
+
+	fmt.Println("Prefix: '" + prefix + "'")
 
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
@@ -38,7 +76,7 @@ func main() {
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
 	// Cleanly close down the Discord session.
@@ -52,13 +90,26 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "pong")
+	// If message does not start with prefix, ignore it
+	if len(m.Content) < len(prefix) || m.Content[0:len(prefix)] != prefix {
+		return
 	}
 
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "ping")
+	// If message is not in a guild, ignore it
+	if m.GuildID == "" {
+		return
 	}
+
+	// Convert message to lowercase
+	message := m.Content[len(prefix):]
+	message = strings.ToLower(message)
+
+	// Convert string containg letters to Int64
+	h := md5.New()
+	io.WriteString(h, message)
+	var seed uint64 = binary.BigEndian.Uint64(h.Sum(nil))
+	rand.Seed(int64(seed))
+
+	// reply to the user with a random reply and notify user
+	s.ChannelMessageSend(m.ChannelID, "<@"+m.Author.ID+">, "+replies[rand.Intn(len(replies))])
 }
